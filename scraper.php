19dd6154fd8540d9a926ef0828a2d5a6a2cf39ed
iimooomii <53833761+iimooomii@users.noreply.github.com>
2019-11-11 14:39:19 +0300

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,64 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+<?php
+require 'scraperwiki.php';
+# Blank PHP
+require 'scraperwiki/simple_html_dom.php';           
+$sportday = new DateTime();
+$sportday->sub(new DateInterval('P1D'));
+    $url = "http://www.zulubet.com/".$sportday->format("Y-m-d")."/";
+    $html = scraperWiki::scrape($url);        
+    $dom = new simple_html_dom();
+    $dom->load($html);
+    foreach($dom->find("content_table") as $data){
+        $home="";
+        $score="";
+        $away="";
+        $odds1 = "";
+        $odds2 = "";
+        $odds3 = "";
+        $pred1="";
+        $pred2="";
+        $pred3="";
+        $prediction="";
+        $record = $data->find("td.home a");    
+        if($record!=null){
+            $home = $record[0]->innertext;            
+        }
+        $record = $data->find("td.score a");    
+        if($record!=null){
+            $score = $record[0]->innertext;            
+        }
+        $record = $data->find("td.away a");    
+        if($record!=null){
+            $away = $record[0]->innertext;            
+        }    
+        $record = $data->find("td.odds");    
+        if($record!=null){
+            $odds1 = $record[0]->innertext;    
+            $odds2 = $record[1]->innertext;    
+            $odds3 = $record[2]->innertext;            
+        }
+        $record = $data->find("td.prob table.prob");
+        if($record!=null){
+            $prediction = $record[0]->title;        
+            $preds = preg_split("/[\s()]+/", $prediction);
+            $pred1=$preds[1];
+            $pred2=$preds[3];
+            $pred3=$preds[5];
+        }
+        if($home!=""){
+            $row = array(
+                "date"=>$sportday,
+                "home"=>$home,
+                "away"=>$away,
+                "score"=>$score,
+                "hodds"=>$odds1,
+                "dodds"=>$odds2,
+                "aodds"=>$odds3,
+                "hpred"=>$pred1,
+                "dpred"=>$pred2,
+                "spred"=>$pred3
+            );            
+            scraperwiki::save(array(), $row);
+        }
+    }
 ?>
